a742fbcdd74ffc5accc5326ca14e3c6d92bde43a
Muhammadbek <galaxylogin@yandex.com>
2022-09-30 01:46:06 +0500

    Major changes 2.0.0 Responsived, optimized and added ux.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let elInputName = elForm.querySelector(".name");
 let elInputSurname = elForm.querySelector(".surname");
 let elInputTel = elForm.querySelector(".tel");
 let elContainer = document.querySelector(".container");
-let elColumn = document.querySelector(".col-8");
+let elColumn = document.querySelector(".contacts-list");
 let newList = document.createElement("ul");
 let elDeleteButton = document.querySelector(".delete");
 let elAlert = document.querySelector(".alert");
@@ -11,31 +11,46 @@ let elbtn = document.querySelector(".btn");
 let contacts =JSON.parse(localStorage.getItem('contactsArray')) || [];
 
 
+// 
+
 //Unordered list style
 newList.classList.add("d-flex","flex-wrap","justify-content-between","rounded");
-elInputTel.setAttribute("value", "+");
 
 
+//Enable delete button if data true
+if (contacts != "") {
+    elDeleteButton.removeAttribute('disabled');
+}
+
+//Form submit function
 elForm.addEventListener("submit", function(evt) {
     evt.preventDefault();
     
-
+    
     //Get input values
     let elInputNameValue = elInputName.value.trim();
     let elInputSurnameValue = elInputSurname.value.trim();
     let elInputTelValue = elInputTel.value.trim();
     
+    
+    //random contact images
+    let image = images[Math.floor(Math.random()*images.length)]
+    console.log(image);
+    
     //Create object
     let obj = {
         id: contacts.length+1,
+        image: image,
         name: elInputNameValue,
         surname: elInputSurnameValue,
         number: elInputTelValue,
     }
-
+    
     //Check numbers to identify
     let num = contacts.findIndex(numbers => numbers.number == obj.number);
     
+
+    //Check data non-empty
     if(obj.name !="" && num) {
         contacts.push(obj);
         window.location.reload();
@@ -46,8 +61,15 @@ elForm.addEventListener("submit", function(evt) {
     createContacts(contacts);
     
     localStorage.setItem('contactsArray', JSON.stringify(contacts));
-    
+
+    if ('contactsArray' in localStorage) {
+        console.log("Yes");
+    }else {
+        console.log("No");
+    }
+      
 });
+
 
 
 //Delete all contacts from list
@@ -75,42 +97,41 @@ function createContacts(arr){
         newParagraphName.classList.add("fw-semibold");
         newParagraphSurName.classList.add("fw-semibold");
         newTel.classList.add("text-decoration-none", "text-dark", "call");
-
-
-        newTel.href= "tel:" +i.number;
-
-        newCallBtn.classList.add("btn", "btn-primary", "d-block", "call-btn");
-        newCallBtn.textContent = "Call";
-        newCallBtn.href= "tel:" + i.number;
-
+        
+        
+        newTel.href= "tel:" + i.number;
+        
+        newCallBtn.classList.add("d-block", "call-btn");
+        newCallBtn.href= "tel:+" + i.number;
+        
         //Contact names
         newSpanName = document.createElement("span", "fw-normal");
         newSpanName.classList.add("span-name", "d-inline");
-
-
+        
+        
         //Adding name to tag
         newParagraphName.appendChild(newSpanName);
         
-
+        
         //Textcontent data
         newSpanName.textContent = "Name: " + i.name;
         newParagraphSurName.textContent = "Relationship: " + i.surname;
-        newTel.textContent = "Tel: " + i.number;
+        newTel.textContent = "Tel: " + "+" + i.number;
         
-
+        
         //Append to div element. In div (p and span)
         newTextContent.appendChild(newParagraphName);
         newTextContent.appendChild(newParagraphSurName);
         
-
-        //Contact image styles
-        newImage.src = "https://picsum.photos/50/50";
-        newImage.style.height = "60px";
-        newImage.classList.add("rounded-circle", "mt-2");
         
-
+        // Contact image styles
+        newImage.src = i.image;
+        newImage.style.height = "60px";
+        newImage.classList.add("rounded-circle", "mt-4");
+        
         //Append image to div. div = (image, p and span)
-        newItemTop.appendChild(newImage); 
+        
+        newItemTop.appendChild(newImage);
         newItemTop.appendChild(newTextContent);
         newItem.appendChild(newItemTop);
         newItem.appendChild(newTel);
---
+++ data.js
@@ -0,0 +1,5 @@
+let images = ["https://picsum.photos/id/1005/50/50", "https://picsum.photos/id/1002/50/50", "https://picsum.photos/id/1070/50/50", "https://picsum.photos/id/1071/50/50", "https://picsum.photos/id/109/50/50", "https://picsum.photos/id/110/50/50", "https://picsum.photos/id/133/50/50" ]
+
+
+
+// let images = ["https://randomuser.me/api/portraits/men/41.jpg", "https://randomuser.me/api/portraits/men/54.jpg", "https://randomuser.me/api/portraits/men/31.jpg", "	https://randomuser.me/api/portraits/men/72.jpg", "https://randomuser.me/api/portraits/men/86.jpg", "https://randomuser.me/api/portraits/men/24.jpg", "https://randomuser.me/api/portraits/men/3.jpg", "https://randomuser.me/api/portraits/men/69.jpg", "https://randomuser.me/api/portraits/men/64.jpg", "https://randomuser.me/api/portraits/men/39.jpg", "https://randomuser.me/api/portraits/men/85.jpg", "https://randomuser.me/api/portraits/men/38.jpg", "https://randomuser.me/api/portraits/men/91.jpg"];
